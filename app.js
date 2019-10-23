442ba7b9a867285334a1f766a9eaebb85dc35787
rhivent <zzzzjrjr@gmail.com>
2019-10-23 11:45:25 +0700

    commit all 2

--- app.js
+++ app.js
@@ -7,8 +7,42 @@ const logging = fn => {
 }
 
 const greetMe = () => {
-    console.log(`Hai Riventus`);
+    // console.log(`Hai Riventus`);
 }
 
 // greetMe();
 logging(greetMe);
+
+// function constructor
+function Person(firstname,lastname){
+    this.fName = firstname
+    this.lName = lastname
+}
+
+// inheritance for js
+Person.prototype.greet = function() {
+    // console.log(`Halo ${this.fName} ${this.lName}`)
+}
+let hehe = new Person('Riventus','aritonang')
+// console.log(hehe.greet());
+// console.log(hehe.__proto__);
+
+
+// pass by value
+function change(b){
+    b=2;
+}
+let a = 1;
+change(a);
+console.log(a);
+
+// pass by reference
+function changeObj(d){
+    d.prop1 = function(){};
+    d.prop2={};
+}
+let c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
+
